2c1bf88c6b6b2a5d23111719726788a0e0ff1b58
Pedro Miguel <pedromiguelmb@gmail.com>
2020-03-19 14:55:11 +0000

    Project creation

---
+++ Sophie/src/EntryPoint.cpp
@@ -0,0 +1,23 @@
+/*!**********************************************************************
+\name			EntryPoint
+\author			Pedro Barreiro
+\date			19/03/2020
+\version		1.0
+*********************************************************************/
+#include "EntryPoint.h"
+#include <iostream>
+
+/*!**********************************************************************
+\name			main
+\author			Pedro Barreiro
+\date			19/03/2020
+\version		1.0
+\param[in]		int argc
+\param[in]		char* argv[]
+*********************************************************************/
+int main(int argc, char* argv[])
+{
+	std::cout << "Sophie" << std::endl;
+	
+	std::cin.get();
+}
